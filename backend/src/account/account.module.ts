8154493f636c0a6e017def989b565da2a5434951
Hong Jong-hwan <wtrdd1@stu.jejunu.ac.kr>
2026-09-13 02:04:05 +0900

    feat(account): wire account security center backend

--- backend/src/account/account.module.ts
+++ backend/src/account/account.module.ts
@@ -3,19 +3,26 @@ import { AuthModule } from '../auth/auth.module';
 import type { Queryable } from '../core/db';
 import { PG_POOL } from '../core/pool.provider';
 import { EncryptionService } from '../security/encryption.service';
+import { AccountSecurityController } from './account-security.controller';
+import { AccountSecurityRepository } from './account-security.repository';
 import { AccountController } from './account.controller';
 import { PostgresAccountRepository } from './account.repository';
 import { AccountService } from './account.service';
 
 @Module({
   imports: [forwardRef(() => AuthModule)],
-  controllers: [AccountController],
+  controllers: [AccountController, AccountSecurityController],
   providers: [
     {
       provide: AccountService,
       inject: [PG_POOL, EncryptionService],
       useFactory: (pool: Queryable | null, encryption: EncryptionService) =>
         pool ? new AccountService(new PostgresAccountRepository(pool, encryption)) : null,
+    },
+    {
+      provide: AccountSecurityRepository,
+      inject: [PG_POOL],
+      useFactory: (pool: Queryable | null) => (pool ? new AccountSecurityRepository(pool) : null),
     },
   ],
   exports: [AccountService],
