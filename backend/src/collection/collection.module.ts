c2fa9e01bd79e8a3a41d6f211d751f90beac02c6
wtrdd1-hash <wtrdd1@gmail.com>
2026-09-23 13:05:45 +0900

    fix(collection): CollectionModule에 AuthModule 임포트 추가하여 SessionGuard 의존성 해결

--- backend/src/collection/collection.module.ts
+++ backend/src/collection/collection.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { CoreModule } from '../core/core.module';
 import { CollectionController } from './collection.controller';
 import { PostgresCollectionRepository } from './collection.repository';
 import { CollectionService } from './collection.service';
 
 @Module({
-  imports: [CoreModule],
+  imports: [CoreModule, AuthModule],
   controllers: [CollectionController],
   providers: [PostgresCollectionRepository, CollectionService],
   exports: [CollectionService],
 })
 export class CollectionModule {}
+
